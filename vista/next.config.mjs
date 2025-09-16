88e7b4472c8ca1f971b4d5332e508ceab0dde2c6
Devam Gupta <devamg759@gmail.com>
2025-09-16 19:56:01 +0530

    msg

--- vista/next.config.mjs
+++ vista/next.config.mjs
@@ -1,4 +1,12 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+  outputFileTracingRoot: __dirname,
+};
 
 export default nextConfig;
