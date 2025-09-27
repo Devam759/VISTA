c53f0660c435d6276c212cdadd210b63b044529a
Devam Gupta <devamg759@gmail.com>
2025-09-27 08:38:33 +0530

    update

--- src/lib/api.js
+++ src/lib/api.js
@@ -40,24 +40,6 @@ function isPointInPolygon(point, polygon) {
     const xi = polygon[i][0], yi = polygon[i][1];
     const xj = polygon[j][0], yj = polygon[j][1];
     
-    // Ray casting algorithm - check if point is inside polygon
-    if (((yi > y) !== (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi)) {
-      inside = !inside;
-    }
-  }
-  
-  return inside;
-}
-
-// Check if point is inside polygon using ray casting algorithm
-function isPointInPolygon(point, polygon) {
-  const x = point[0], y = point[1];
-  let inside = false;
-  
-  for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
-    const xi = polygon[i][0], yi = polygon[i][1];
-    const xj = polygon[j][0], yj = polygon[j][1];
-    
     if (((yi > y) !== (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi)) {
       inside = !inside;
     }
